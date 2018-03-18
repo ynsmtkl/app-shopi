6cd17b10b03b2af0b30f31a008a068af7d60827e
YNS MTKL <yns.mtkl@gmail.com>
2018-03-18 11:55:01 +0000

    Account model and related files

---
+++ app/models/account.rb
@@ -0,0 +1,8 @@
+class Account < ApplicationRecord
+
+	validates_presence_of :shopify_account_url
+	validates_presence_of :shopify_api_key
+	validates_presence_of :shopify_password
+	validates_presence_of :shopify_shared_secret
+
+end
